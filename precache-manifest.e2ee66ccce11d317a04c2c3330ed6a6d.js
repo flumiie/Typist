e8b0b07d29e3df935156cf40a85d8a1270dcea5a
Ferick Andrew <ferickwar@gmail.com>
2019-07-28 00:16:15 +0700

    Updates

---
+++ precache-manifest.e2ee66ccce11d317a04c2c3330ed6a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2f113d10631c5c48e04c08e8453ab4",
+    "url": "/typist/index.html"
+  },
+  {
+    "revision": "b8cfc72c2749a39acbba",
+    "url": "/typist/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/typist/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b8cfc72c2749a39acbba",
+    "url": "/typist/static/js/main.75f9133c.chunk.js"
+  },
+  {
+    "revision": "96dc86011fca612537bd",
+    "url": "/typist/static/js/runtime~main.d64f5b95.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/typist/static/media/logo.5d5d9eef.svg"
+  }
+]);
