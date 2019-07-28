e9471d3e7347746056af80a367944f6a1a24fcb6
Ferick Andrew <ferickwar@gmail.com>
2019-07-28 11:53:33 +0700

    Typing checker

--- public/scripts.js
+++ public/scripts.js
@@ -1,20 +1,47 @@
+var typedWord, typedChar, toType, counter = 1;
+function wordsToString()
+{
+    toType = [];
+    if($('.to-type span p#' + counter).css('background-color') != 'rgb(0, 128, 0)' ||
+       $('.to-type span p#' + counter).css('background-color') != 'rgb(255, 0, 0)')
+        toType.push($('.to-type span p#' + counter).html())
+    else
+        toType = toType.splice((counter-1), 1)
+}
+
 $(document).ready(function()
 {
-    var typedWord, typedChar, toType
+    var spl = $('.to-type span').html().split(' ');
+    temp = []
+    wordsToString()
+
+    for(var i = 0; i < spl.length; i++)
+    {
+        temp.push('<p id="' + (i+1) + '">' + spl[i] + '</p> ')
+    }
+    $('.to-type span').html(temp)
+
     $('#typing-box input').keypress(function()
     {
         if(event.which == 32 || event.keyCode == 32)
         {
             $('#typing-box input').val('')
-            for(var i = 0; i < toType.length; i++)
+            wordsToString()
+            // console.log(toType)
+            if(typedWord == '') null
+            else
             {
-                if(typedChar[i] == ' ' && toTypePerChar[i] == ' ')
+                if(typedWord != undefined)
                 {
-                    a
-                }
-                else if(typedChar[i] == toTypePerChar[i])
-                {
-                    // if($())
+                    if(typedWord == $('.to-type span p#' + counter).html())
+                    {
+                        $('.to-type span p#' + (counter)).css('background-color', 'green')
+                    }
+                    else
+                    {
+                        $('.to-type span p#' + (counter)).css('background-color', 'red')
+                    }
+                    counter++
                 }
             }
         }
@@ -24,7 +51,7 @@ $(document).ready(function()
     {
         typedWord = $('#typing-box input').val().trim().split(' ')
         typedChar = $('#typing-box input').val().trim().split('')
-        toType = $('.to-type span').html()
+        wordsToString()
 
         if(typedChar.length == 0)
             $('#typing-box input').css('background-color', 'white')
@@ -32,7 +59,7 @@ $(document).ready(function()
         {
             for(var i = 0; i < typedChar.length; i++)
             {
-                if(typedChar[i] == toType[i])
+                if(typedChar[i] == toType[0].split('')[i])
                     $('#typing-box input').css('background-color', 'white')
                 else
                 {
--- src/App.js
+++ src/App.js
@@ -4,39 +4,45 @@ import './assets/css/App.css';
 import WordAnim from 'react-random-word';
 
 var randomWord = require('random-words');
-var rando = randomWord({ exactly: 18, min: 1, max: 9, join: ' ' });
-while(rando.length != 117)
-{
-  rando = randomWord({ exactly: 18, min: 1, max: 9, join: ' ' });
-}
 
-function Main() {
-  return (
-    <div className="container">
-      <header className="header">
-        <div className="whole-bag-of-jellybean">
-          <div className="options">
-            Options
-          </div>
-          <div className="typing-container">
-            <div className="to-type">
-              <WordAnim
-                word={ rando }
-                speed={75}
-                rounds={5}
-                letters="0123456789~!@#$%^&*()_+ &nbsp;"
-              />
+export default class Main extends React.Component {
+  wordRenderer = () =>
+  {
+    var generator = randomWord({ exactly: 18, min: 1, max: 9, join: ' ' })
+    while(generator.length !== 117)
+    {
+      generator = randomWord({ exactly: 18, min: 1, max: 9, join: ' ' })
+    }
+    return generator
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <header className="header">
+          <div className="whole-bag-of-jellybean">
+            <div className="options">
+              Options
             </div>
-            <div id="typing-box">
-              <input type="text" />&nbsp;
-              <button id="redo">↻</button>
+            <div className="typing-container">
+              <div className="to-type">
+                {/* <WordAnim
+                  word={ rando }
+                  speed={75}
+                  rounds={5}
+                  letters="0123456789~!@#$%^&*()_+ &nbsp;"
+                /> */}
+                <span>{ this.wordRenderer() }</span>
+              </div>
+              <div id="typing-box">
+                <input type="text" />&nbsp;
+                <button id="redo">↻</button>
+              </div>
+              
             </div>
-            
           </div>
-        </div>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
-
-export default Main;
