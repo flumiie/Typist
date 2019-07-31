4046fea29355398df80418bdaaf91f10b6d61019
Ferick Andrew <ferickwar@gmail.com>
2019-07-31 14:57:15 +0700

    for loop with var for performance

--- src/App.js
+++ src/App.js
@@ -81,9 +81,10 @@ export default class Main extends React.Component {
               </div>
             </div>
             <div id="typing-box">
-              <input type="text"/>
-              {/* <input type="text" disabled placeholder="Still under maintenance, stay updated!" style={{textAlign:'center',textTransform:'uppercase'}}/> */}
-              <button id="redo" onClick={ this.wordRenderer }>↻</button>
+              {/* <input type="text"/> */}
+              {/* <button id="redo" onClick={ this.wordRenderer }>↻</button> */}
+              <input type="text" disabled placeholder="Still under maintenance, stay updated!" style={{textAlign:'center',textTransform:'uppercase'}}/>
+              <button id="redo" onClick={ this.wordRenderer } disabled>↻</button>
             </div>
           </div>
         </header>
