01d0041fc2e98978ec381341aa34f7c494dcfb95
Ferick Andrew <ferickwar@gmail.com>
2019-08-08 12:59:41 +0700

    Word separation #1

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,37 +1,37 @@
-import React, { Component } from 'react'
-import $ from 'jquery'
+// import React, { Component } from 'react'
+// import $ from 'jquery'
 
-let typedWord, typedChar, toType, lineCount = 1, counter = 1
-let correctWords = [], wrongWords = [], userTyped = [], spl, temp = []
-let typedOncePerRedo = false, timerInterval = null, timer = 60
+// let typedWord, typedChar, toType, lineCount = 1, counter = 1
+// let correctWords = [], wrongWords = [], userTyped = [], spl, temp = []
+// let typedOncePerRedo = false, timerInterval = null, timer = 60
 
-class Scripts extends Component
-{
-    constructor(props)
-    {
-        super(props)
+// class Scripts extends Component
+// {
+//     constructor(props)
+//     {
+//         super(props)
         
-        this.init()
-        this.scripts = this.scripts.bind(this)
-    }
+//         this.init()
+//         this.scripts = this.scripts.bind(this)
+//     }
 
-    init = () =>
-    {
-        var tCache = localStorage.getItem('timer')
-        if(tCache == null || isNaN(tCache))
-        {
-            timer = 60
-            localStorage.setItem('timer', timer)
-        }
-        else
-            timer = tCache
-    }
+//     init = () =>
+//     {
+//         var tCache = localStorage.getItem('timer')
+//         if(tCache == null || isNaN(tCache))
+//         {
+//             timer = 60
+//             localStorage.setItem('timer', timer)
+//         }
+//         else
+//             timer = tCache
+//     }
 
-    scripts = () =>
-    {
-        document.querySelector('.timer').addEventListener('clcik', function(){ console.log('test hv') })
-        $('.timer').on('click', function(){
-            console.log('hover')
-        })
-    }
-}
+//     scripts = () =>
+//     {
+//         document.querySelector('.timer').addEventListener('clcik', function(){ console.log('test hv') })
+//         $('.timer').on('click', function(){
+//             console.log('hover')
+//         })
+//     }
+// }
